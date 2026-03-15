7b32e7ccad8ac4f69e84b9d81475526a5a6209d6
Khaled <kdalmutairi2030@gmail.com>
2026-03-16 02:49:17 +0300

    Edited LibraryItem class

--- src/lpp/item/LibraryItem.java
+++ src/lpp/item/LibraryItem.java
@@ -1,13 +1,49 @@
 package lpp.item;
 
-public class LibraryItem {
-	private int pages;
+import lpp.Displayable;
+import lpp.account.User;
+
+public abstract class LibraryItem implements Displayable{
+	protected String name;
+	protected int pages;
+	protected boolean isAvailable;
+	protected User usedBy;
 	
 	public LibraryItem(int pages) {
 		this.pages = pages;
+		isAvailable= true;
+		usedBy= null;
 	}
-	
+
 	public int getPages() {
 		return pages;
 	}
+	
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	
+	public void useItem() {
+		if (isAvailable)
+		isAvailable= false;
+		else
+		System.out.println(name+ " isn't available!");
+	}
+	
+	public void returnItem() {
+		if(isAvailable== false)
+		isAvailable= true;
+		else
+			System.out.print(name+ " is already available!");
+	}
+	
+	public void setUser(User u) {
+		usedBy= u;
+	}
+	
+	public User getUser() {
+		return usedBy;
+	}
+	
+	public abstract double calculatePrice();
 }
