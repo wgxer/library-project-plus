366da286f5d9a30e78b397cc3f24a6e2ff85da33
wgxer <123264539+wgxer@users.noreply.github.com>
2026-03-18 15:29:27 +0300

    Added empty implementations temporarily to allow program to compile

--- src/lpp/account/Author.java
+++ src/lpp/account/Author.java
@@ -1,5 +1,7 @@
 package lpp.account;
 
 public class Author extends User {
-
+	public Author(String username, String password, double balance) {
+		super(username, password, balance);
+	}
 }
--- src/lpp/item/Manuscript.java
+++ src/lpp/item/Manuscript.java
@@ -1,9 +1,17 @@
 package lpp.item;
 
 public class Manuscript extends LibraryItem {
-
-	public Manuscript(int pages) {
-		super(pages);
+	public Manuscript(int pages, String name, String authorName) {
+		super(pages, name, authorName);
+	}
+	
+	public void display() {
+		// TODO: Implement Manuscript::display
+	}
+	
+	public double calculatePrice() {
+		// TODO: Implement Manuscript::calculatePrice
+		return 0;
 	}
 
 }
